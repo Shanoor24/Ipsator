31587cf48dc93e70b697a11db4eaf483703263a2
Shanoor24 <shanoorfb2403@gmail.com>
2021-09-19 17:12:20 +0530

    navbar done

---
+++ assignment/src/Components/NavBar/NavBar.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import styles from "./NavBar.module.css";
+import {Botton, Button} from "@material-ui/core"
+
+function NavBar() {
+    return (
+        <div id={styles.nav_main_cont}>
+            <div className={styles.logo}>
+                <img src="https://i.imgur.com/fsKSR4z.png" alt="logo" style={{width: "190px", position: "absolute", marginTop: "-30px"}} />
+            </div>
+            <div className={styles.home}>
+                Home
+            </div>
+            <div>
+                <Button variant="contained" size="small" style={{backgroundColor:"#FFFFFF", fontWeight: "700", color: "#000000", marginLeft: "320px", marginRight: "25px", marginTop: "11px"}}>Login</Button>
+            </div>
+            <div>
+                <Button  variant="contained" color="white" size="small" style={{backgroundColor:"#FFFFFF", fontWeight: "700", color: "#000000", marginTop: "11px"}}>Sign-Up</Button>
+            </div>
+        </div>
+    )
+}
+
+export default NavBar;
--- assignment/src/Route/Routes.jsx
+++ assignment/src/Route/Routes.jsx
@@ -1,10 +1,11 @@
 import React from "react";
+import NavBar from "../Components/NavBar/NavBar";
 
 
 function Routes() {
     return (
         <div>
-
+            <NavBar />
         </div>
     )
 }
